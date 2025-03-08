562beee36470a085ee97411bb00baf8b1c61ff7f
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2025-03-07 23:18:46 -0300

    Testes na classe quadrado

---
+++ testes/CirculoTest.java
@@ -0,0 +1,27 @@
+import junit.framework.TestCase;
+
+public class CirculoTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testGetRaio() {
+
+    }
+
+    public void testSetRaio() {
+    }
+
+    public void testGetArea() {
+    }
+
+    public void testGetPerimetro() {
+    }
+
+    public void testGetTipoFigura() {
+    }
+
+    public void testCompareTo() {
+    }
+}
---
+++ testes/QuadradoTest.java
@@ -0,0 +1,41 @@
+import junit.framework.TestCase;
+
+public class QuadradoTest extends TestCase {
+
+    public void setUp() throws Exception {
+        Quadrado quadrado2 = new Quadrado(3);
+        super.setUp();
+    }
+
+    public void testGetLado() {
+        Quadrado quadrado = new Quadrado(4);
+        assertEquals(4, quadrado.getLado(), 0.0001);
+    }
+
+    public void testSetLado() {
+        Quadrado quadrado = new Quadrado(3);
+        quadrado.setLado(4);
+        assertEquals(4, quadrado.getLado(), 0.0001);
+    }
+
+    public void testGetArea() {
+        Quadrado quadrado = new Quadrado(4);
+        assertEquals(16, quadrado.getArea(), 0.0001);
+    }
+
+    public void testGetPerimetro() {
+        Quadrado quadrado = new Quadrado(3);
+        assertEquals(12, quadrado.getPerimetro(), 0.0001);
+    }
+
+    public void testGetTipoFigura() {
+        Quadrado quadrado = new Quadrado(3);
+        assertEquals("Quadrado", quadrado.getTipoFigura());
+    }
+
+    public void testCompareTo() {
+        Quadrado q1 = new Quadrado(3);
+        Quadrado q2 = new Quadrado(4);
+        assertTrue(q1.compareTo(q2) < 0);
+    }
+}
